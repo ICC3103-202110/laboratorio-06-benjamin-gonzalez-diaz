79f1ea8b4093183e34f90eced898140f2fa58965
benjamin gonzalez <bigonzalez@miuandes.cl>
2021-05-18 21:44:47 -0400

    mejoras

--- update.js
+++ update.js
@@ -4,17 +4,26 @@ function CelsiusKelvin(temp){
 function CelsiusFahrenheit(temp){
     return parseFloat((temp * 9/5) + 32)
 }
+function CelsiusRankine(temp){
+    return parseFloat((temp * 9/5 + 491.67 ))
+}
 function KelvinFahrenheit(temp){
     return parseFloat((temp - 273.15) * 9/5 + 32)
 }
 function KelvinCelsius(temp){
     return parseFloat(temp - 273.15) 
 }
+function KelvinRankine(temp){
+    return parseFloat((temp * 1.8))
+}
 function FahrenheitKelvin(temp){
     return parseFloat(( temp - 32) * 5/9 + 273.15)
 }
 function FahrenheitCelsius(temp){
     return parseFloat(( temp - 32) * 5/9)
+}
+function FahrenheitRankine(temp){
+    return parseFloat((temp + 459.67))
 }
 function Update(option,temperature,unit,convert,Model){
     if(unit === 'Celsius' && convert === 'Fahrenheit'){
@@ -23,17 +32,35 @@ function Update(option,temperature,unit,convert,Model){
     else if(unit === 'Celsius' && convert === 'Kelvin'){
         NewTemp = CelsiusKelvin(temperature)
     }
+    else if(unit === 'Celsius' && convert === ' '){
+        NewTemp = CelsiusRankine(temperature)
+    }
     else if(unit === 'Kelvin' && convert === 'Celsius'){
         newTemp = KelvinCelsius(temperature)
     }
     else if(unit === 'Kelvin' && convert === 'Fahrenheit'){
         NewTemp = KelvinFahrenheit(temperature)
     }
+    else if(unit === 'Kelvin' && convert === ' '){
+        NewTemp = KelvinRankine(temperature)
+    }
     else if(unit === 'Fahrenheit' && convert === 'Kelvin'){
         NewTemp = FahrenheitKelvin(temperature)
     }
     else if(unit === 'Fahrenheit' && convert === 'Celsius'){
         NewTemp = FahrenheitCelsius(temperature)
+    }
+    else if(unit === 'Fahrenheit' && convert === ' '){
+        NewTemp = FahrenheitRankine(temperature)
+    }
+    else if(unit === ' ' && convert === 'Celsius'){
+        NewTemp = ((temperature - 491.67) * 5/9)
+    }
+    else if(unit === ' ' && convert === 'Kelvin'){
+        NewTemp = (temperature *  5/9)
+    }
+    else if(unit === ' ' && convert === 'Fahrenheit'){
+        NewTemp = (temperature - 459.67 )
     }
     else{
         NewTemp = temperature
